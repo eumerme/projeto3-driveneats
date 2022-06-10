13abf65d3b73498360712050f1e4eb405b3078f0
eumerme <euthaisetau@gmail.com>
2022-06-10 11:30:03 -0300

    feat: adição da selação dos menus

---
+++ scripts.js
@@ -0,0 +1,103 @@
+function aviso () {
+    alert ("Visualização mobile em 414px :)");
+}
+aviso();
+
+/* borda check pratos */
+function checkBox1 () {
+    const check1 = document.querySelector("#um");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#um ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox2 () {
+    const check1 = document.querySelector("#dois");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#dois ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox3 () {
+    const check1 = document.querySelector("#três");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#três ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox4 () {
+    const check1 = document.querySelector("#quatro");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#quatro ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+/* borda check bebidas */
+function checkBox5 () {
+    const check1 = document.querySelector("#cinco");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#cinco ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox6 () {
+    const check1 = document.querySelector("#seis");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#seis ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox7 () {
+    const check1 = document.querySelector("#sete");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#sete ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox8 () {
+    const check1 = document.querySelector("#oito");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#oito ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+/* borda check sobremesa */
+function checkBox9 () {
+    const check1 = document.querySelector("#nove");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#nove ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox10 () {
+    const check1 = document.querySelector("#dez");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#dez ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox11 () {
+    const check1 = document.querySelector("#onze");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#onze ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
+
+function checkBox12 () {
+    const check1 = document.querySelector("#doze");
+	check1.classList.toggle("bordaCheck");
+
+    const check2 = document.querySelector("#doze ion-icon");
+	check2.classList.toggle("ioniconCheck");
+}
