280208885fc9f4b7e506aab95a1d89012a5ee76a
eumerme <euthaisetau@gmail.com>
2022-06-12 16:06:07 -0300

    update: link de enviar mensagem

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
-function aviso () {
-    alert ("Visualização mobile em 414px :)");
-}
-aviso();
-
-
-
-/* borda check pratos */
+/*selecionar pratos */
 function checkBoxP (elemento) {
     const pratoClicado = document.querySelector(".pratos .bordaCheck");
 
@@ -18,8 +11,18 @@ function checkBoxP (elemento) {
     pedido();
 }
 
+function ionCheckP (elemento){    
+    const iconeCheck = elemento.querySelector(".pratos .icone-ionicon");
+    const check = document.querySelector(".pratos .ionicon-check");
+    
+    if (check !== null){
+        check.classList.remove("ionicon-check");
+    }
+    iconeCheck.classList.add("ionicon-check");
+}
 
-/* borda check bebidas */
+
+/* selecionar bebidas */
 function checkBoxB (elemento) {
     const bebidaClicado = document.querySelector(".bebidas .bordaCheck");
 
@@ -32,7 +35,17 @@ function checkBoxB (elemento) {
     pedido();
 } 
 
-/* borda check sobremesas */
+function ionCheckB (elemento){    
+    const iconeCheck = elemento.querySelector(".bebidas .icone-ionicon");
+    const check = document.querySelector(".bebidas .ionicon-check");
+    
+    if (check !== null){
+        check.classList.remove("ionicon-check");
+    }
+    iconeCheck.classList.add("ionicon-check");
+}
+
+/* slecionar sobremesas */
 function checkBoxS (elemento) {
     const sobremesaClicado = document.querySelector(".sobremesas .bordaCheck");
 
@@ -45,18 +58,37 @@ function checkBoxS (elemento) {
     pedido();    
 } 
 
+function ionCheckS(elemento){    
+    const iconeCheck = elemento.querySelector(".sobremesas .icone-ionicon");
+    const check = document.querySelector(".sobremesas .ionicon-check");
+    
+    if (check !== null){
+        check.classList.remove("ionicon-check");
+    }
+    iconeCheck.classList.add("ionicon-check");
+}    
+
 /* botão fechar pedido */
 function pedido() {
+    const pratoEscolhido = document.querySelector(".pratos .bordaCheck");
+    const bebidaEscolhida = document.querySelector(".bebidas .bordaCheck");
+    const sobremesaEscolhida = document.querySelector(".sobremesas .bordaCheck");
 
-    let pedidoAberto = document.querySelector(".barra-base .botao-3itens");
-    let pedidoFechado = document.querySelector(".barra-base .fecharPedido");
+    const botao3itens = document.querySelector(".barra-base .botao-3itens");
+    const botaoFecharPedido = document.querySelector(".barra-base .fecharPedido");
 
-    let pratoEscolhido = document.querySelector(".pratos .bordaCheck");
-    let bebidaEscolhido = document.querySelector(".bebidas .bordaCheck");
-    let sobremesaEscolhido = document.querySelector(".sobremesas .bordaCheck");
-
-    if (pratoEscolhido && bebidaEscolhido && sobremesaEscolhido) { 
-        pedidoAberto.classList.add("escondido");
-        pedidoFechado.classList.remove("escondido");
+    if (pratoEscolhido && bebidaEscolhida && sobremesaEscolhida) { 
+        botaoFecharPedido.classList.remove("escondido");
+        botao3itens.classList.add("escondido");
     }
+}
+
+//alterar nomessss 
+
+function msgWpp(){
+
+    const msg = `Olá, gostaria de fazer o pedido:\n- Prato: Frango Yin Yang\n- Bebida: Coquinha Gelada\n- Sobremesa: Pudim\nTotal: R$ 27.70`;
+    const enviarMsg = `https://wa.me/5524988235658?text=${encodeURIComponent(msg)}`;
+    window.open(enviarMsg);
+    
 }
